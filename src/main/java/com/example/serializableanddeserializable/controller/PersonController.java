7ec238458fba8e4506695a934bdaeb3677d4db08
LRLayan <rameshlayan4@gmail.com>
2024-07-17 17:00:45 +0530

    deserializable code example using json

--- src/main/java/com/example/serializableanddeserializable/controller/PersonController.java
+++ src/main/java/com/example/serializableanddeserializable/controller/PersonController.java
@@ -2,6 +2,9 @@ package com.example.serializableanddeserializable.controller;
 
 import java.io.*;
 
+import com.example.serializableanddeserializable.dto.PersonDTO;
+import jakarta.json.bind.Jsonb;
+import jakarta.json.bind.JsonbBuilder;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
@@ -20,6 +23,13 @@ public class PersonController extends HttpServlet {
 
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response)throws IOException{
+        if (!request.getContentType().toLowerCase().startsWith("application/json") || request.getContentType() == null){
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+        }
 
+        //deserializable
+        Jsonb jsonb = JsonbBuilder.create();
+        PersonDTO personDTO = jsonb.fromJson(request.getReader(), PersonDTO.class);
+        System.out.println(personDTO);
     }
 }
